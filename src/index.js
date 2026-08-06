2a99de305fb4fe6befa36e9f06dd55a08030aec7
Taha Mahamoud Sougueh <44029588+taha891@users.noreply.github.com>
2026-08-06 17:02:49 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
